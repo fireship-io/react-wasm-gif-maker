d2489dce3189f860e539e85c3e676398b6880fb1
Jeff Delaney <hello@fireship.io>
2020-11-15 10:17:14 -0700

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,26 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
