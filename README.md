b181a0b33af11ffbe80c4a8b1b932fe83dcfd861
Jeff Delaney <hello@fireship.io>
2020-11-16 14:28:47 -0700

    full working demo

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# New Project
+# GIF Maker with WASM (FFMpeg) & React
+
+Convert a video file to an animated GIF with Web Assembly. 
+
+Watch the full tutorial on YouTube and read WASM React tutorial on Fireship.io. 
 
 > ✨ Bootstrapped with Create Snowpack App (CSA).
-
-## Available Scripts
 
 ### npm start
 
@@ -12,14 +14,3 @@ Open http://localhost:8080 to view it in the browser.
 The page will reload if you make edits.
 You will also see any lint errors in the console.
 
-### npm run build
-
-Builds a static copy of your site to the `build/` folder.
-Your app is ready to be deployed!
-
-**For the best production performance:** Add a build bundler plugin like "@snowpack/plugin-webpack" to your `snowpack.config.js` config file.
-
-### npm test
-
-Launches the application test runner.
-Run with the `--watch` flag (`npm test -- --watch`) to run in interactive watch mode.
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
+
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
+    load();
+  }, [])
+
+  const convertToGif = async () => {
+    // Write the file to memory 
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+    // Run the FFMpeg command
+    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+    // Read the result
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    // Create a URL
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+    setGif(url)
+  }
+
+  return ready ? (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      { video && <video
+        controls
+        width="250"
+        src={URL.createObjectURL(video)}>
+
+      </video>}
+
+
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+
+      <h3>Result</h3>
+
+      <button onClick={convertToGif}>Convert</button>
+
+      { gif && <img src={gif} width="250" />}
+
     </div>
-  );
+  )
+    :
+    (
+      <p>Loading...</p>
+    );
 }
 
 export default App;
